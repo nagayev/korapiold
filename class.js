2b34dcf64cbac3ecf97692b728562992940eee17
Marat Nagayev <sidorovmarat1995@gmail.com>
2018-05-10 21:11:49 +0300

    Крупное обновление апи

--- class.js
+++ class.js
@@ -5,31 +5,18 @@ const request = require('sync-request');
 const querystring = require('querystring');
 const color = require('chalk'); //for debug
 const assert = require('assert').equal; //for debug
-//error class (for api)
-/*
-class KorError{
-  constructor(arg){
-    console.warn(color.bold.red("Error:KorError"));
-  }
-}
-*/
-
+const fs = require('fs-sync'); //for read cookie file
 //for debug
-var red=arg=>console.log(color.bold.red(arg));
-var green=arg=>console.log(color.bold.green(arg));
+var error=arg=>console.log(color.bold.red("ERROR:" + arg));
+var normal=arg=>console.log(color.bold.green(arg));
+var warn=arg=>console.log(color.bold.yellow("WARNING:" + arg));
 
-var nick="YOUR_NICK"; //можно const
+var nick="tirzon"; //можно const
 
-class Api{
+class KorApi{
     constructor(){
-        this.cookie={
-            last_logged_user_customer_id:'', //editable
-            __utma:'133173652.540521041.1516119582.1516119582.1516119582.1',
-            __utmz:'133173652.1516119582.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
-            _ym_uid:'1516119582257435681', //const
-            sbsid:'', //editable
-            tzo:-3 //const
-        }
+        this.cookie=JSON.parse(fs.read('cookie.json'));
+        this.cookie.tzo=new Date().getTimezoneOffset()/60; //устанавливаем нужную зону
         this.cookie=this.create_cookie(this.cookie);
         this.baseurl='http://' + nick + '.kor.ru/';
     }
@@ -57,12 +44,13 @@ class Api{
         }
         if(body!=null) options.body=body; //if body exists,add body
         var r=request(method,url,options);
-        console.log(r.headers);
-        if(body==null) assert(r.url,this.baseurl) //auth nick test
+        //console.log(r.headers);
+        if(body==null && header=={}) assert(r.url,this.baseurl) //auth nick test
         return r;
     }
-    sendPost(message){
+    sendPost(obj){
         var url=`http://${nick}.kor.ru/blog/add/ajax_text/`;
+        /*
         var body={
             comments_idea:0, //0 это разрешено,1 это запрет
             message_id:0, //new post
@@ -71,9 +59,10 @@ class Api{
             who_idea:2, //[all,friends,me]
             tags:"" //tags
         };
+        */
+        obj.message_id=0;
         var headers={};
-        var body=querystring.stringify(body);
-        //headers['body']=body;
+        var body=querystring.stringify(obj);
         headers['Refer']=`http://${nick}.kor.ru/`;
         headers['Host']=`${nick}.kor.ru`;
         //options['headers']['Content-Length']=Buffer.byteLength(body);
@@ -82,6 +71,21 @@ class Api{
         console.log(body);
         var r=this.req('POST',url,body,headers);
         console.log(r.body.toString('utf-8'));
+        return r;
+    }
+    deletePost(id){
+        var url = `http://${nick}.kor.ru/blog/delete/${id}`;
+        var headers={};
+        headers['Refer']=`http://${nick}.kor.ru/`;
+        headers['Host']=`${nick}.kor.ru`;
+        headers['X-Requested-With']='XMLHttpRequest';
+        var body = null; //no body
+        var r = this.req('POST',url,body,headers);
+        var r_body = r.body.toString('utf-8');
+        //баг на стороне кора:ok даже если поста уже нет
+        if(r_body.indexOf('ok') == -1) warn("Error in deletePost");
+        console.log(r_body);
+        return r;
     }
     sendMood(mood){
         var url = `http://${nick}.kor.ru/blog/add/ajax_mood/`;
@@ -94,6 +98,20 @@ class Api{
         headers['X-Requested-With']='XMLHttpRequest';
         var r = this.req('POST',url,body,headers);
         console.log(r.body.toString('utf-8'));
+        return r;
+    }
+    deleteMood(){
+        var url = `http://${nick}.kor.ru/blog/delete/ajax_mood/`;
+        var body = {mood:''};
+        var body = querystring.stringify(body);
+        var headers={};
+        headers['Content-Type']='application/x-www-form-urlencoded; charset=UTF-8';
+        headers['Refer']=`http://${nick}.kor.ru/`;
+        headers['Host']=`${nick}.kor.ru`;
+        headers['X-Requested-With']='XMLHttpRequest';
+        var r = this.req('POST',url,body,headers);
+        console.log(r.body.toString('utf-8'));
+        return r;
     }
     sendFriendRequest(user,id){
         var url = `http://${user}.kor.ru/relationship/add_friend_request/`;
@@ -104,7 +122,49 @@ class Api{
         headers['Host']=`${user}.kor.ru`;
         headers['X-Requested-With']='XMLHttpRequest';
         var r = this.req('POST',url,body,headers);
-        console.log(r.body.toString('utf-8'));
+        var r_body = r.body.toString('utf-8');
+        console.log(r_body);
+        if(r_body.indexOf('error')>-1) warn("Error in friend request");
+        return r;
+    }
+    deletePhotoAlbum(id){
+        var url = `http://${nick}.kor.ru/photoalbums/delete_album/?album_id=${id}`;
+        var headers = {};
+        headers['Refer']=`http://${nick}.kor.ru/photoalbums/`;
+        headers['Host']=`${nick}.kor.ru`;
+        headers['X-Requested-With']='XMLHttpRequest';
+        var body = null;
+        var r = this.req('GET',url,body,headers);
+        var r_body = r.body.toString('utf-8');
+        console.log(r_body);
+        if(r_body.indexOf('error')>-1) warn("Error in deletePhotoAlbum");
+        return r;
+    }
+    addtoFavourites(post_id){
+        var _ = +new Date; //time(milliseconds)
+        var url = `http://${nick}.kor.ru/blog/add_to_favourites/?message_id=${post_id}&_=${_}`;
+        var body = null;
+        var headers={};
+        headers['Refer']=`http://${nick}.kor.ru/blog/my_favourites/p1`;
+        headers['Host']=`${nick}.kor.ru`;
+        headers['X-Requested-With']='XMLHttpRequest';
+        var r = this.req('GET',url,body,headers);
+        var r_body = r.body.toString('utf-8');
+        console.log(r_body);
+        return r;
+    }
+    deletefromFavourites(post_id){
+        var _ = +new Date; //time(milliseconds)
+        var url = `http://${nick}.kor.ru/blog/remove_from_favourites/?message_id=${post_id}&_=${_}`;
+        var body = null;
+        var headers={};
+        headers['Refer']=`http://${nick}.kor.ru/blog/my_favourites/p1`;
+        headers['Host']=`${nick}.kor.ru`;
+        headers['X-Requested-With']='XMLHttpRequest';
+        var r = this.req('GET',url,body,headers);
+        var r_body = r.body.toString('utf-8');
+        console.log(r_body);
+        return r;
     }
     create_cookie(cookie){
         var tmp="";
@@ -112,8 +172,11 @@ class Api{
         return tmp.slice(0,-2); //delete the last '; '
     }
 }
-var test = new Api();
-test.req(); 
-test.sendPost("Hello!");
-test.sendMood("Mood");
-test.sendFriendRequest("demfarbar",5078762); //1 аргумент это ник,второй его id
+//var test = new KorApi();
+//test.req(); //auth using cookie
+//test.sendPost("Hello!");
+//test.sendMood("Mood");
+//test.sendFriendRequest("demfarbar",5078762);
+//test.deletePhotoAlbum(1);
+//test.deletePost(2807800);
+exports.korapi=KorApi; //НЕ УДАЛЯТЬ
